31da1d334431c9287e10652665ebd96ef7e90fb2
kuderr <dakudryavcev@gmail.com>
2020-01-21 16:18:23 +0300

    server docker fixes

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,5 @@
 docker stop nvr_autorecord
 docker rm nvr_autorecord
 docker build -t nvr_autorecord .
-docker run -idt --name nvr_autorecord --env-file .env nvr_autorecord
+docker run -idt --name nvr_autorecord --net=host --env-file .env nvr_autorecord
+
