0afc9638cd1eab6f63b453dd112b2cc1f3b39e96
kuderr <dakudryavcev@gmail.com>
2020-01-23 15:00:27 +0300

    model db fix

--- models.py
+++ models.py
@@ -8,23 +8,29 @@ Base = declarative_base()
 class Room(Base):
     __tablename__ = 'rooms'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100), nullable=False)
-    free = Column(Boolean, default=True)
-    tracking_state = Column(Boolean, default=False)
-    timestamp = Column(Integer, default=0)
-    chosen_sound = Column(String(100), default='enc')
-    sources = relationship('Source', backref='room', lazy=False)
-    drive = Column(String(200))
-    calendar = Column(String(200))
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    free = db.Column(db.Boolean, default=True)
+    tracking_state = db.Column(db.Boolean, default=False)
+
+    sound_source = db.Column(db.String(100), default='0')
+    main_source = db.Column(db.String(100), default='0')
+    tracking_source = db.Column(db.String(100), default='0')
+    screen_source = db.Column(db.String(100), default='0')
+
+    sources = db.relationship('Source', backref='room', lazy=False)
+    drive = db.Column(db.String(200))
+    calendar = db.Column(db.String(200))
 
     def to_dict(self):
         return dict(id=self.id,
                     name=self.name,
                     free=self.free,
                     tracking_state=self.tracking_state,
-                    timestamp=self.timestamp,
-                    chosen_sound=self.chosen_sound,
+                    sound_source=self.sound_source,
+                    main_source=self.main_source,
+                    tracking_source=self.tracking_source,
+                    screen_source=self.screen_source,
                     sources=[source.to_dict() for source in self.sources],
                     drive=self.drive,
                     calendar=self.calendar)
@@ -33,27 +39,18 @@ class Room(Base):
 class Source(Base):
     __tablename__ = 'sources'
 
-    id = Column(Integer, primary_key=True)
-    ip = Column(String(200))
-    name = Column(String(100))
-    sound = Column(String(100))
-    tracking = Column(Boolean, default=False)
-    main_cam = Column(Boolean, default=False)
-    room_id = Column(Integer, ForeignKey('rooms.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    ip = db.Column(db.String(200), default='0')
+    name = db.Column(db.String(100), default='источник')
+    room_id = db.Column(db.Integer, db.ForeignKey('rooms.id'))
 
     def __init__(self, **kwargs):
-        self.ip = kwargs.get('ip', "0.0.0.0")
-        self.name = kwargs.get('name', 'камера')
-        self.sound = kwargs.get('sound')
-        self.tracking = kwargs.get('tracking', False)
-        self.main_cam = kwargs.get('main_cam', False)
+        self.ip = kwargs.get('ip')
+        self.name = kwargs.get('name')
         self.room_id = kwargs.get('room_id')
 
     def to_dict(self):
         return dict(id=self.id,
                     ip=self.ip,
                     name=self.name,
-                    sound=self.sound,
-                    tracking=self.tracking,
-                    main_cam=self.main_cam,
                     room_id=self.room_id)
