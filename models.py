f5b3bf6095b27223e4787d7454aca30219266bf7
burnouttt <burnouttt@yandex.ru>
2020-02-24 19:16:59 +0300

    New DB models

--- models.py
+++ models.py
@@ -15,44 +15,61 @@ class Room(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
     tracking_state = Column(Boolean, default=False)
-
-    sound_source = Column(String(100), default='0')
-    main_source = Column(String(100), default='0')
-    tracking_source = Column(String(100), default='0')
-    screen_source = Column(String(100), default='0')
-
     sources = relationship('Source', backref='room', lazy=False)
     drive = Column(String(200))
     calendar = Column(String(200))
+
+    sound_source = Column(String(100))
+    main_source = Column(String(100))
+    tracking_source = Column(String(100))
+    screen_source = Column(String(100))
+
+    auto_control = Column(Boolean, default=True)
 
     def to_dict(self):
         return dict(id=self.id,
                     name=self.name,
                     tracking_state=self.tracking_state,
+                    sources=[source.to_dict() for source in self.sources],
+                    drive=self.drive,
+                    calendar=self.calendar,
                     sound_source=self.sound_source,
                     main_source=self.main_source,
                     tracking_source=self.tracking_source,
                     screen_source=self.screen_source,
-                    sources=[source.to_dict() for source in self.sources],
-                    drive=self.drive,
-                    calendar=self.calendar)
+                    auto_control=self.auto_control)
 
 
 class Source(Base):
     __tablename__ = 'sources'
 
     id = Column(Integer, primary_key=True)
-    ip = Column(String(200), default='0')
     name = Column(String(100), default='источник')
+    ip = Column(String(200))
+    port = Column(String(200))
+    rtsp = Column(String(200), default='no')
+    audio = Column(String(200))
+    merge = Column(String(200))
+    tracking = Column(String(200))
     room_id = Column(Integer, ForeignKey('rooms.id'))
 
     def __init__(self, **kwargs):
-        self.ip = kwargs.get('ip')
         self.name = kwargs.get('name')
+        self.ip = kwargs.get('ip')
+        self.port = kwargs.get('port')
+        self.rtsp = kwargs.get('rtsp')
+        self.audio = kwargs.get('audio')
+        self.merge = kwargs.get('merge')
+        self.tracking = kwargs.get('tracking')
         self.room_id = kwargs.get('room_id')
 
     def to_dict(self):
         return dict(id=self.id,
-                    ip=self.ip,
                     name=self.name,
+                    ip=self.ip,
+                    port=self.port,
+                    rtsp=self.rtsp,
+                    audio=self.audio,
+                    merge=self.merge,
+                    tracking=self.tracking,
                     room_id=self.room_id)
