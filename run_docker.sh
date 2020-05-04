c9ea26c8920720c3bbffc125eceec022208ea326
kuderr <dakudryavcev@gmail.com>
2020-05-04 15:03:26 +0300

    nvr env get

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 docker stop nvr_autorecord
 docker rm nvr_autorecord
 docker build -t nvr_autorecord .
-docker run -idt --name nvr_autorecord --net=host --env-file .env nvr_autorecord
+docker run -idt --name nvr_autorecord --net=host --env-file ../.env_nvr nvr_autorecord
