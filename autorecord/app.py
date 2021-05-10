ebc8cc34edb861b70dc982fb6124932a502c1a00
kuderr <dakudryavcev@gmail.com>
2021-05-10 15:13:31 +0300

    delete files

--- autorecord/app.py
+++ autorecord/app.py
@@ -77,7 +77,7 @@ class Autorecord:
                 for source in recorder.room.sources
             ]
         )
-        # await self._loop.run_in_executor(None, Cleaner.clear_sound, recorder)
+        await self._loop.run_in_executor(None, Cleaner.clear_sound, recorder)
 
     async def process_source(self, recorder, source, folder_id):
         """Обработка источника"""
@@ -86,10 +86,10 @@ class Autorecord:
 
         await AudioMapper.map_video_and_sound(recorder, source)
         file_id = await Uploader.upload(recorder, source, folder_id)
-        # await Publisher.send_to_erudite(recorder, source, file_id)
+        await Publisher.send_to_erudite(recorder, source, file_id)
 
-        # await self._loop.run_in_executor(None, Cleaner.clear_video, recorder, source)
-        # await self._loop.run_in_executor(None, Cleaner.clear_result, recorder, source)
+        await self._loop.run_in_executor(None, Cleaner.clear_video, recorder, source)
+        await self._loop.run_in_executor(None, Cleaner.clear_result, recorder, source)
 
 
 if __name__ == "__main__":
